f38fa7eff984703ac0792842b84ace5a5ada83ea
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2025-04-09 22:38:03 -0500

    Tipos de Datos, entero, float y boleano

---
+++ 5.Enteros-Flotantes-Booleanos/clase5.py
@@ -0,0 +1,31 @@
+# TIPO ENTERO(integer(class int))
+x = 10
+print(x)
+print(type(x))
+
+# TIPO FLOTANTE(float(class float))
+y = 5.678
+print(y)
+print(type(y))
+
+# NOTACION CIENTIFICA, TOMAR EN CUENTA QUE COLOCA 5 CEROS Y DE AHI TOMA EN CUENTA EL NUMERO DEPUES DEL "." ENTONCES CONTARIA 6 CON ESE
+# z = 1.2E6
+# print(z)
+
+
+# a = 1.2E-6
+# print(a)
+
+# OPERACIONES
+print(x + x)
+print(x + y)
+print(y + y)
+
+
+# TIPO BOOLEANO(boolean(class bool))
+is_true = True
+is_False = False
+
+print(is_true)
+print(is_False)
+print(type(is_False))
