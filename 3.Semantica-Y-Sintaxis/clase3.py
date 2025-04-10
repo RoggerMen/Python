1bd87ceed8bc83a120933c4e1dea0828cd382a88
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2025-04-09 21:19:16 -0500

    Introduccion Hola mundo Python y tener buenas practicas con la sintaxis y semantica

---
+++ 3.Semantica-Y-Sintaxis/clase3.py
@@ -0,0 +1,12 @@
+# ESTO ES PARA VER EL SISTEMA - VERSION de python
+import sys
+print(sys.version)
+
+saludo = "Hola"
+nombre = "Rogger"
+print(saludo)
+
+print(f"{saludo} Mundo")
+print("Hola Mundo2")
+print(f"{saludo} {nombre}")
+
